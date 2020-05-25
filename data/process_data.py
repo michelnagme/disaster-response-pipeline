15d378eaccfe4ba08c22f2f3a16b331499c73461
Michel Francisco Nagme Rachid Costa <michelfnrc@gmail.com>
2020-05-24 21:18:09 -0300

    Adding ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,34 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    return messages.merge(categories, on='id')
 
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';', expand=True)
+    category_colnames = categories.loc[0].apply(lambda x: x[:-2]).tolist()
+    categories.columns = category_colnames
+
+    for column in categories:
+        categories[column] = categories[column].str[-1:]
+        categories[column] = pd.to_numeric(categories[column])
+
+    df.drop('categories', axis=1, inplace=True)
+
+    df = pd.concat([df, categories], axis=1)
+
+    return df.drop_duplicates(inplace=True)
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('disaster_messages', engine, index=False)
 
 
 def main():
